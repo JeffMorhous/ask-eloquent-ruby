4a311eb2fb320a74e06b0403a67641e08b26c7db
Jeff Morhous <morhous.jeff@gmail.com>
2023-04-17 14:24:21 -0400

    Begin scripting pdf to embeddings

---
+++ script/format_pdf_as_embeddings.rb
@@ -0,0 +1,13 @@
+# TODO: Open the provided file
+
+# TODO: For each page in the book, get the content, page number, and number of tokens of that content
+
+# TODO: Put that in a CSV, probably
+
+# TODO: Create an embedding for each row in the csv using the OpenAI Embeddings API.
+
+# TODO: Return a dictionary that maps between each embedding vector and the index of the row that it corresponds to.
+
+# TODO: Create an embeddings CSV with exactly these named columns:
+# "title", "0", "1", ... up to the length of the embedding vectors.
+
