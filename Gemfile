32bac36661370d6e4850f35a7f8e2f49f314f14c
Jeff Morhous <morhous.jeff@gmail.com>
2023-04-17 16:31:21 -0400

    Write script to convert pdf into vector embeddings

--- Gemfile
+++ Gemfile
@@ -48,6 +48,13 @@ gem "bootsnap", require: false
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
+# Used for format_pdf_as_embeddings.rb
+gem "pdf-reader"
+gem "words_counted"
+gem "pandas"
+gem "httparty"
+gem "csv"
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- script/format_pdf_as_embeddings.rb
+++ script/format_pdf_as_embeddings.rb
@@ -1,13 +1,72 @@
-# TODO: Open the provided file
+require 'pdf-reader'
+require 'words_counted'
+require 'httparty'
+require 'csv'
 
-# TODO: For each page in the book, get the content, page number, and number of tokens of that content
+def count_tokens(text)
+  WordsCounted.count(text).token_count
+end
+def extract_pages(page_text, index)
+  return [] if page_text.empty?
 
-# TODO: Put that in a CSV, probably
+  content = page_text.split.join(' ')
+  puts "page text: #{content}"
+  outputs = [["Page #{index}", content, count_tokens(content) + 4]]
 
-# TODO: Create an embedding for each row in the csv using the OpenAI Embeddings API.
+  outputs
+end
 
-# TODO: Return a dictionary that maps between each embedding vector and the index of the row that it corresponds to.
+pdf_file_name = ARGV[0]
+filename = "#{pdf_file_name}"
 
-# TODO: Create an embeddings CSV with exactly these named columns:
-# "title", "0", "1", ... up to the length of the embedding vectors.
+reader = PDF::Reader.new(filename)
 
+res = []
+i = 1
+reader.pages.each do |page|
+  res += extract_pages(page.text, i)
+  i += 1
+end
+
+# Filter out rows with tokens greater than or equal to 2046
+filtered_res = res.reject { |row| row[2] >= 2046 }
+
+# Write the filtered results to a CSV file
+CSV.open("#{filename}.pages.csv", 'wb') do |csv|
+  csv << ['title', 'content', 'tokens']
+  filtered_res.each { |row| csv << row }
+end
+
+def get_embedding(text, model)
+  openai_api_key = ENV['OPEN_AI_API_KEY']
+  response = HTTParty.post(
+    'https://api.openai.com/v1/embeddings',
+    headers: { 'Authorization' => "Bearer #{openai_api_key}", 'Content-Type' => 'application/json' },
+    body: { 'model': model, 'input': text }.to_json
+  )
+  puts response
+  response['data'][0]['embedding']
+end
+
+def get_doc_embedding(text)
+  model_name = 'curie'
+  get_embedding(text, "text-search-#{model_name}-doc-001")
+end
+
+def compute_doc_embeddings(data)
+  embeddings = {}
+  data.each_with_index do |row, idx|
+    content = row[1]
+    embeddings[idx] = get_doc_embedding(content)
+  end
+  embeddings
+end
+
+doc_embeddings = compute_doc_embeddings(filtered_res)
+
+CSV.open("#{filename}.embeddings.csv", 'wb') do |csv|
+  csv << ['title'] + (0...4096).to_a
+  doc_embeddings.each do |i, embedding|
+    csv << ["Page #{i + 1}"] + embedding
+  end
+end
